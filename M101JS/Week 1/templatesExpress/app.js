a81a7969791eb3a317df940e331a84fba893ceac
ahmad riyadh al faathin <riyadh11@live.com>
2018-03-26 18:54:41 +0700

    First Commit

---
+++ M101JS/Week 1/templatesExpress/app.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    app =   express(),
+    engines = require('consolidate'),
+    mongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+app.engine('html',engines.nunjucks);
+app.set('view engine','html');
+app.set('views',__dirname+'/views');
+
+mongoClient.connect('mongodb://localhost:27017/video', function(err,db){
+    assert.equal(null,err);
+    console.log("Succesfully connected to MongoDB.");
+    
+    app.get('/',function(req,res){
+        db.collection('movies').find({}).toArray(function(err,docs){
+            res.render('movies',{'movies':docs});
+        });
+    });
+
+    app.use(function(req,res){
+        res.sendStatus(404);
+    });
+});
+
+var server = app.listen(3000,function(){
+    var port = server.address().port;
+    console.log('Express server listening on port %s',port);
+});
